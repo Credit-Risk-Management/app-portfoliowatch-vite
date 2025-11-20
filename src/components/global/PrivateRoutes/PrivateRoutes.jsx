9be92a0c2d6f5b45ff6bfb385e3468f0d966db1b
justinffortier <justinffortier@gmail.com>
2025-11-19 22:55:19 -0800

    chore: lint

--- src/components/global/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/global/PrivateRoutes/PrivateRoutes.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Outlet, Navigate } from 'react-router-dom';
 import { $global } from '@src/signals';
 import ContentWrapper from '../ContentWrapper';
@@ -6,9 +7,11 @@ const PrivateRoutes = () =>
 // if (!$global.value.isSignedIn && !$global.value.isLoading) {
 //   return <Navigate to={`/?redirect=${window.location.pathname}`} />;
 // }
+// eslint-disable-next-line indent, implicit-arrow-linebreak
 (
   <ContentWrapper fluid className="min-vh-100 bg-info-900">
     <Outlet />
   </ContentWrapper>
+  // eslint-disable-next-line indent
 );
 export default PrivateRoutes;
