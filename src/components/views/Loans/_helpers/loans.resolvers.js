cc1c03e47a0798c4e804e5574bebb3947d91c29c
cgillette12 <cgillette1312@gmail.com>
2026-03-26 14:54:57 -0700

    Updated UI

--- src/components/views/Loans/_helpers/loans.resolvers.js
+++ src/components/views/Loans/_helpers/loans.resolvers.js
@@ -181,6 +181,7 @@ export const fetchLoanDetail = async (loanId, options = {}) => {
   try {
     $loan.update({ isLoading: true });
     $watchScoreBreakdown.update({ isLoading: true });
+    $loanDetailMissingFinancials.value = false;
 
     await loadLoanDetailData(loanId);
 
